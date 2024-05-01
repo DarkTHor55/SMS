243f01a7780d3d27a13b8c9215e5aff327dee529
DarkTHor55 <negir166661@gmail.com>
2024-05-01 18:36:48 +0530

    ca

---
+++ frontend/src/Components/Home/HomeCarouselData.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export const HomeCarouselData=[
+    {
+        image:'https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2/6/26_1.jpg'
+    },
+    {
+        image:'https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/9/19_2.jpg'
+    },
+    {
+        image:'https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2/8/28.jpg'
+    },
+    {
+        image:'https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/0/1/01_saree_coll_copy_7.jpg'
+    },
+]
