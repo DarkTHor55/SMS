af1d173ee01930e851902d07ba78dd296860e90c
Abhay Aswal <abhayaswal939@gamil.com>
2024-05-03 10:42:51 +0530

    updated UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,16 +3,20 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className="fixed -[999] w-full px-20 py-8 flex justify-between items-center">
+    <div className="fixed w-full px-20 py-8 flex justify-between items-center  top-0 left-0  bg-white shadow-lg p-4">
        <div className='logo'>
-      <div className='font-semibold uppercase text-5xl'>SWS</div>
+          <div className='font-semibold uppercase text-5xl'>SWS</div>
        </div>
-       <div className="links flex gap-10">
+       <div className="links flex gap-10" style={{ 
+           '@media screen and (max-width: 768px)': {
+             gap: '5px', /* Adjust as needed */
+           }
+         }}>
             <Link className='text-md capitalize font-semibold' to={"/"}>Home</Link>
             <Link className='text-md capitalize font-semibold' to={"/about"}>About us</Link>
             <Link className='text-md capitalize font-semibold' to={"/blogs"}>Blogs</Link>
             <Link className='text-md capitalize font-semibold' to={"/events"}>Events</Link>
-            <Link className='text-md capitalize font-semibold' to={"/contact"}>contact</Link>
+            <Link className='text-md capitalize font-semibold' to={"/contact"}>Contact</Link>
        </div>
     </div>
   )
